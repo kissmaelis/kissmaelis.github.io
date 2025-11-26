945f595798bad67ac52e6ef89cda5499836e7589
kissmaelis <kissmaelis@proton.me>
2025-11-26 17:57:32 +0100

    fx

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,9 @@
-import { activityPubPlugin } from 'eleventy-plugin-activity-pub';
+const activityPubPlugin = require('eleventy-plugin-activity-pub');
 
 export default async function(eleventyConfig) {
 	// Configure Eleventy
 	eleventyConfig.addPlugin(activityPubPlugin, {
-		domain: 'kissmaelis.com',
+		domain: 'blog.kissmaelis.com',
 		username: 'maelis',
 		displayName: 'Maelis Gast',
 		summary: 'My website on the fediverse!',
