4b9f639c29b0b221469671ac49e5bd7448d7ca60
kissmaelis <kissmaelis@proton.me>
2025-11-26 18:04:22 +0100

    remove package

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
 //const activityPubPlugin = require('eleventy-plugin-activity-pub');
-import {activityPubPlugin} from 'eleventy-plugin-activity-pub';
+//import {activityPubPlugin} from 'eleventy-plugin-activity-pub';
 
 export default async function(eleventyConfig) {
 	// Configure Eleventy
-	eleventyConfig.addPlugin(activityPubPlugin, {
+/* 	eleventyConfig.addPlugin(activityPubPlugin, {
 		domain: 'blog.kissmaelis.com',
 		username: 'maelis',
 		displayName: 'Maelis Gast',
 		summary: 'My website on the fediverse!',
-	});
+	}); */
 };
